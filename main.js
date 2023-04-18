737e2944342bdcf86a889088e18e0e3465e32af2
theNkennaAmadi <108593101+theNkennaAmadi@users.noreply.github.com>
2023-04-18 03:47:15 +0100

    console test

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { Flip } from "gsap/Flip";
 
 gsap.registerPlugin(ScrollTrigger, Flip);
 
-console.log("now we are talking 2");
+console.log("now we are talking 3 ");
 
 gsap.to(".box", {
   scrollTrigger: {
